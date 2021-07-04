60c0c6560283bdcb19871c1f299f6979d74edd7c
Carla Lutz Dias <carladias@nasajon.com.br>
2021-07-04 18:36:40 -0300

    Criacao de arquivos para o levantamento do projeto

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -520,7 +520,6 @@ class ComposerStaticInit041b2deaee794b390ccd170ce9c9e84e
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
-        'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
